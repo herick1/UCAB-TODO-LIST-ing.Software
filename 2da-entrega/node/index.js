413c64e32cd0fcea47443f8a2c44d7c7f0001b51
herick1 <herick200@gamil.com>
2017-12-16 08:42:14 -0400

    index.js ultimos cambios

--- 2da-entrega/node/index.js
+++ 2da-entrega/node/index.js
@@ -4,14 +4,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-
-
 const app = express()
 const port = process.env.PORT || 3001
-
-
-app.use(bodyParser.urlencoded({ extended:false }))
-app.use(bodyParser.json())
 
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
@@ -22,9 +16,11 @@ var collection;
 var f = new Date();
 var fechaa = f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear() +" "+ f.getHours() + ":" + f.getMinutes() + ":" + f.getSeconds()  ;
 
+app.use(bodyParser.urlencoded({ extended:false }))
+app.use(bodyParser.json())
 
 MongoClient.connect(url, (err,res)=>{
- assert.equal(null, err);
+    assert.equal(null, err);
     if(err){
         console.log("Coudln't connect to mongo. Error"+err);
     } else{
@@ -38,89 +34,72 @@ MongoClient.connect(url, (err,res)=>{
 });
 
 app.get('/v1/todos',(req,res) => {
-  console.log('GET /v1/todos')
-  collection.find({}).toArray(function(err, todos) {
-    assert.equal(err, null);
-    if(err) return res.status(500).send({ message:' hubo un error al hacer la peticion' })
-    if(!todos) return res.status(404).send({ message:' no existe' })
-    res.status(200).json(todos);
-  })
+    console.log('GET /v1/todos')
+    collection.find({}).toArray(function(err, todos) {
+        assert.equal(err, null);
+        if(err) return res.status(500).send({ mensaje:' hubo un error al hacer la peticion' })
+        if(!todos) return res.status(404).send({ mensaje:' no existe' })
+        res.status(200).json(todos);
+    })
 })
 
-
-
 app.post('/v1/todos', (req, res) => {
-
-  console.log('POST /v1/todos')
-  console.log(req.body)
-  collection.insert({nombre:req.body.nombre, descripcion:req.body.descripcion, lista:null, completada:false, fecha:fechaa},
+    console.log('POST /v1/todos')
+    console.log(req.body)
+    collection.insert({nombre:req.body.nombre, descripcion:req.body.descripcion, lista:null, completada:false, fecha:fechaa},
     function(err, docs) {
-    // Tests unitarios
-    assert.equal(err, null);
-    assert.equal(1, docs.result.n);
-    assert.equal(1, docs.ops.length);
-      // Log de consola
-      console.log("Insertada la colección.");
-      res.status(200).send(req.body)
-  })
-
-
+            assert.equal(err, null);
+            assert.equal(1, docs.result.n);
+            assert.equal(1, docs.ops.length);
+            console.log("Insertada la colección.");
+            res.status(200).json(docs.ops)
+            if(err) return res.status(500).send({ mensaje:' hubo un error al hacer la peticion' })
+    })
 })
 
 app.delete('/v1/todos/:id',(req,res) => {
-
-  console.log('DELETE /v1/todos/:id')
-   collection.deleteOne({_id: new mongodb.ObjectID(req.params.id)}, function(err, results) {
-    assert.equal(err, null);
-     if(err) return res.status(500).send({ message:' hubo un error al hacer la peticion' })
-     if(!results) return res.status(404).send({ message:' no exste' })
-     console.log("eliminada la colección.")
-     res.status(200).send({ message:' eliminado con exito' })
-  })
+    console.log('DELETE /v1/todos/:id')
+    collection.deleteOne({_id: new mongodb.ObjectID(req.params.id)}, function(err, results) {
+        assert.equal(err, null);
+        if(err) return res.status(500).send({ mensaje:' hubo un error al hacer la peticion' })
+        if(!results) return res.status(404).send("TODO no encontrado")
+        console.log("eliminada la colección.")
+        res.status(200).send("Eliminado")
+    })
 })
 
 app.put('/v1/todos/:id',(req,res) => {
-
-  console.log('PUT /v1/todos/:id')
-  var completa;
-  if(req.body.completada == "false")completa=false;
-  if(req.body.completada == "true") completa=true;
-   collection.update({_id: new mongodb.ObjectID(req.params.id)},
+    console.log('PUT /v1/todos/:id')
+    var completa;
+    if(req.body.completada == "false")completa=false;
+    if(req.body.completada == "true") completa=true;
+    collection.update({_id: new mongodb.ObjectID(req.params.id)},
     { $set: {nombre:req.body.nombre, descripcion:req.body.descripcion, completada:completa}}
-     , function(err, results) {
-    assert.equal(err, null);
-     if(err) return res.status(500).send({ message:' hubo un error al hacer la peticion' })
-     if(!results) return res.status(404).send({ message:' no exste' })
-     console.log("actualizada la colección.")
-     res.status(200).json(results)
-  })
+    ,function(err, results) {
+        assert.equal(err, null);
+        if(err) return res.status(500).send({ mensaje:' hubo un error al hacer la peticion' })
+        if(!results) return res.status(404).send("TODO no entontrado")
+        console.log("actualizada la colección.")
+        res.status(200).send("Modificado")
+     })
 })
-
 
 app.put('/v1/todos/:id/alterar-completado',(req,res) => {
-
-  console.log('PUT /v1/todos/:id/alterar-completado')
-  collection.find({_id: new mongodb.ObjectID(req.params.id)}).toArray(function(err, todos) {
-  if(err) return res.status(500).send({ message:' hubo un error al hacer la peticion' })
-  if(!todos) return res.status(404).send({ message:' no exste' })
-  var completa = false;
-  if(todos[0].completada == false)completa=true;
-  if(todos[0].completada == true) completa=false;
-   collection.update({_id: new mongodb.ObjectID(req.params.id)},
-    { $set: {completada:completa}}
-     , function(err, results) {
-           assert.equal(err, null);
-     if(err) return res.status(500).send({ message:' hubo un error al hacer la peticion' })
-     if(!results) return res.status(404).send({ message:' no exste' })
-     console.log("actualizada la colección.")
-     res.status(200).send({ message:' actualizado' })
-  })
-
-  })
-
-})
-
-//en caso de que lleve parametros (parecido a lo anterior)
-app.get('/hola/:name',(req,res) => {       //esto es lo que se cambia por la ruta GET
-  res.send({ messade:`HOLA ${req.params.name} `})  //esto devolvera el json con el parametro que se diga
+    console.log('PUT /v1/todos/:id/alterar-completado')
+    collection.find({_id: new mongodb.ObjectID(req.params.id)}).toArray(function(err, todos) {
+          if(err) return res.status(500).send({ mensaje:' hubo un error al hacer la peticion' })
+          if(!todos) return res.status(404).send("TODO no entontrado")
+          var completa;
+          if(todos[0].completada == false)completa=true;
+          if(todos[0].completada == true) completa=false;
+          collection.update({_id: new mongodb.ObjectID(req.params.id)},
+          { $set: {completada:completa}}
+          ,function(err, results) {
+                assert.equal(err, null);
+                if(err) return res.status(500).send({ mensaje:' hubo un error al hacer la peticion' })
+                if(!results) return res.status(404).send("TODO no entontrado")
+                console.log("actualizada la colección.")
+                res.status(200).send("Alterado")
+          })
+    })
 })
